5baeac2fd133b5c1e6e0213b3ea4bff8db8cf82a
shingoasou-0804 <122670383+shingoasou-0804@users.noreply.github.com>
2024-12-30 15:38:11 +0900

    modify env variables

--- main.py
+++ main.py
@@ -10,17 +10,36 @@ from google.oauth2 import service_account
 
 
 load_dotenv(override=True)
-PROJECT_ID = os.environ.get("PROJECT_ID", "generative-ai-app-426413")
-TOPIC_ID = os.environ.get("TOPIC_ID", "terraform-test-app-log-v2")
-SERVICE_ACCOUNT_FILE_PATH = os.environ.get("SERVICE_ACCOUNT_FILE_PATH", "service_account.json")
-credentials = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE_PATH)
+PROJECT_ID = os.environ.get("PROJECT_ID")
+TOPIC_ID = os.environ.get("TOPIC_ID")
+PRIVATE_KEY_ID = os.environ.get("PRIVATE_KEY_ID")
+PRIVATE_KEY = os.environ.get("PRIVATE_KEY").replace("\\n", "\n")
+CLIENT_EMAIL = os.environ.get("CLIENT_EMAIL")
+CLIENT_ID = os.environ.get("CLIENT_ID")
+CLIENT_X509_CERT_URL = os.environ.get("CLIENT_X509_CERT_URL")
 
 app = Flask(__name__)
+
+credentials = service_account.Credentials.from_service_account_info(
+    {
+        "type": "service_account",
+        "project_id": PROJECT_ID,
+        "private_key_id": PRIVATE_KEY_ID,
+        "private_key": PRIVATE_KEY,
+        "client_email": CLIENT_EMAIL,
+        "client_id": CLIENT_ID,
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        "token_uri": "https://oauth2.googleapis.com/token",
+        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+        "client_x509_cert_url": CLIENT_X509_CERT_URL,
+        "universe_domain": "googleapis.com"
+    }
+)
+publisher = pubsub_v1.PublisherClient(credentials=credentials)
 
 
 @app.route("/")
 def publish():
-    publisher = pubsub_v1.PublisherClient(credentials=credentials)
     topic_path = publisher.topic_path(PROJECT_ID, TOPIC_ID)
     log_data = _get_log_data()
     publisher.publish(topic_path, json.dumps(log_data).encode('utf-8'))
